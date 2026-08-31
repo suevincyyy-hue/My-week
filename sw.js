3e4544f809d1c9e89911fcd2e5be8d70f998de9f
suevincyyy-hue <suevincyyy@gmail.com>
2026-08-31 12:05:45 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='river-weekly-0.23';const A=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
